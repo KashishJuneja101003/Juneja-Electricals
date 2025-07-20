6cb8140533d6244a4653a73f83c55ce4ba9490c5
Kashish Juneja <69794677+KashishJuneja101003@users.noreply.github.com>
2025-07-20 18:44:51 +0530

    cashfree async added

--- src/utils/loadCashFree.js
+++ src/utils/loadCashFree.js
@@ -1,14 +1,22 @@
-// src/utils/loadCashFree.js
 export const loadCashfreeSDK = () => {
   return new Promise((resolve, reject) => {
     if (window.Cashfree && typeof window.Cashfree.checkout === "function") {
-      return resolve(); // already loaded
+      resolve();
+      return;
     }
 
     const script = document.createElement("script");
     script.src = "https://sdk.cashfree.com/js/ui/2.0.0/dropin.min.js";
-    script.onload = () => resolve();
-    script.onerror = () => reject("Cashfree SDK failed to load");
+    script.async = true;
+    script.onload = () => {
+      console.log("✅ Cashfree SDK loaded");
+      resolve();
+    };
+    script.onerror = (e) => {
+      console.error("❌ SDK failed to load", e);
+      reject("Cashfree SDK failed to load");
+    };
+
     document.body.appendChild(script);
   });
 };
