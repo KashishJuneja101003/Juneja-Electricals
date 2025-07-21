0bd0e8886e0bf8274ce7437e947236a668d1bd74
Kashish Juneja <69794677+KashishJuneja101003@users.noreply.github.com>
2025-07-21 17:54:15 +0530

    Corrected path to access .env in all files

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,6 +1,8 @@
+require("dotenv").config({path: "../.env"})
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
+  console.log(process.env.JWT_SECRET);
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) return res.status(401).json({ message: "No token provided" });
@@ -12,6 +14,7 @@ const authMiddleware = (req, res, next) => {
   } catch (error) {
     res.status(403).json({ message: "Invalid token" });
   }
+
 };
 
 module.exports = authMiddleware;
