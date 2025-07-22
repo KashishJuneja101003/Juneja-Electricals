28658b41cc41bfad5dc07a88085f4860b0c18a77
Kashish Juneja <69794677+KashishJuneja101003@users.noreply.github.com>
2025-07-22 18:07:08 +0530

    Added code according to the version

--- components/OrderGateway.jsx
+++ components/OrderGateway.jsx
@@ -70,7 +70,7 @@ const OrderGateway = () => {
       const cashfreeInstance = new window.Cashfree();
 
       cashfreeInstance.initialiseDropin(dropinContainerRef.current,{
-        paymentSessionId: sessionId, // ✅ correct
+        orderToken: sessionId, // ✅ correct
         components: ["card", "upi", "upi-qrcode", "netbanking"],
         style: {
           theme: "light",
