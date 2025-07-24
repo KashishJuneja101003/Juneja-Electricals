40c2800b91041cc33240c8d352b914678e56009b
Kashish Juneja <69794677+KashishJuneja101003@users.noreply.github.com>
2025-07-24 19:12:06 +0530

    Removed files added drive links for legal documentation

--- components/Legal.jsx
+++ components/Legal.jsx
@@ -6,21 +6,21 @@ const Legal = () => {
         <h2 className="text-2xl md:text-3xl mb-2 text-start font-semibold">Legal</h2>
         <div className="flex justify-center space-x-6 bg-[#e1d9d9] p-3">
           <a
-            href="/Contact.pdf"
+            href="https://drive.google.com/file/d/1ycEqg2tSbNn0M5v5nacuF89g55sghyXa/view?usp=drive_link"
             download="Contact.pdf"
             className="text-blue-600 underline hover:text-blue-800"
           >
             Contact Us
           </a>
           <a
-            href="/Terms.pdf"
+            href="https://drive.google.com/file/d/12msSTwWMV-1eZtgG_zNPtE67Egq2zRrS/view?usp=drive_link"
             download
             className="text-blue-600 underline hover:text-blue-800"
           >
             Terms & Conditions
           </a>
           <a
-            href="/RefundPolicy.pdf"
+            href="https://drive.google.com/file/d/1Y01T2WqAk8qsQ29L56ih50FhiB8UkmEe/view?usp=drive_link"
             download
             className="text-blue-600 underline hover:text-blue-800"
           >
