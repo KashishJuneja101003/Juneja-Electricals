ecd04be675595681b5b7913e847cbc3b64e1d698
Kashish Juneja <69794677+KashishJuneja101003@users.noreply.github.com>
2025-08-09 11:16:04 +0530

    Update Product Page - fields location adjusted

--- components/UpdateProduct.jsx
+++ components/UpdateProduct.jsx
@@ -88,118 +88,133 @@ const UpdateProduct = () => {
   }));
 
   return (
-    <div className="p-4">
-      <h3 className="text-xl font-bold mb-4">Update Product</h3>
+    <div className="flex justify-center text-green-800">
+      <div className="flex justify-center items-center p-2 flex-col  border-2 border-sky-300 rounded-2xl w-fit gap-2 bg-sky-200">
+        <h3 className="text-3xl font-semibold text-center">Update Product</h3>
+        <hr className="text-sky-300 w-full" />
 
-      {/* Product dropdown with images */}
-      <Select
-        options={options}
-        onChange={(option) => handleSelectProduct(option.product)}
-        placeholder="Select a product"
-        className="w-fit"
-      />
+        <div className="p-4">
+          {/* Product dropdown with images */}
+          <Select
+            options={options}
+            onChange={(option) => handleSelectProduct(option.product)}
+            placeholder="Select a product"
+            className="w-fit"
+          />
 
-      {selectedProduct && (
-        <div>
-          <div className="grid grid-cols-2 gap-2 mt-4">
-            <input
-              className="border p-2"
-              placeholder="Name"
-              value={updatedProduct.name}
-              onChange={(e) =>
-                setUpdatedProduct({ ...updatedProduct, name: e.target.value })
-              }
-            />
+          {selectedProduct && (
+            <div>
+              <div className="grid grid-cols-2 gap-2 mt-4">
+                <input
+                  className="border p-2"
+                  placeholder="Name"
+                  value={updatedProduct.name}
+                  onChange={(e) =>
+                    setUpdatedProduct({
+                      ...updatedProduct,
+                      name: e.target.value,
+                    })
+                  }
+                />
 
-            <img src={updatedProduct.imageUrl} className="w-40" alt="" />
+                <input
+                  className="border p-2"
+                  placeholder="Category"
+                  value={updatedProduct.category}
+                  onChange={(e) =>
+                    setUpdatedProduct({
+                      ...updatedProduct,
+                      category: e.target.value,
+                    })
+                  }
+                />
 
-            <input
-              className="border p-2"
-              placeholder="Category"
-              value={updatedProduct.category}
-              onChange={(e) =>
-                setUpdatedProduct({
-                  ...updatedProduct,
-                  category: e.target.value,
-                })
-              }
-            />
+                <input
+                  className="border p-2"
+                  placeholder="Price"
+                  value={updatedProduct.price}
+                  onChange={(e) =>
+                    setUpdatedProduct({
+                      ...updatedProduct,
+                      price: e.target.value,
+                    })
+                  }
+                />
 
-            <input
-              className="border p-2"
-              placeholder="Image URL"
-              value={updatedProduct.imageUrl}
-              onChange={(e) =>
-                setUpdatedProduct({
-                  ...updatedProduct,
-                  imageUrl: e.target.value,
-                })
-              }
-            />
+                <input
+                  className="border p-2"
+                  placeholder="Quantity"
+                  type="number"
+                  value={updatedProduct.quantity}
+                  onChange={(e) =>
+                    setUpdatedProduct({
+                      ...updatedProduct,
+                      quantity: e.target.value,
+                    })
+                  }
+                />
 
-            <input
-              className="border p-2"
-              placeholder="Price"
-              value={updatedProduct.price}
-              onChange={(e) =>
-                setUpdatedProduct({ ...updatedProduct, price: e.target.value })
-              }
-            />
+                <input
+                  className="border p-2"
+                  placeholder="Features"
+                  value={updatedProduct.features}
+                  onChange={(e) =>
+                    setUpdatedProduct({
+                      ...updatedProduct,
+                      features: e.target.value,
+                    })
+                  }
+                />
 
-            <input
-              className="border p-2"
-              placeholder="Quantity"
-              type="number"
-              value={updatedProduct.quantity}
-              onChange={(e) =>
-                setUpdatedProduct({
-                  ...updatedProduct,
-                  quantity: e.target.value,
-                })
-              }
-            />
+                <input
+                  className="border p-2"
+                  placeholder="Image URL"
+                  value={updatedProduct.imageUrl}
+                  onChange={(e) =>
+                    setUpdatedProduct({
+                      ...updatedProduct,
+                      imageUrl: e.target.value,
+                    })
+                  }
+                />
 
-            <input
-              className="border p-2"
-              placeholder="Features"
-              value={updatedProduct.features}
-              onChange={(e) =>
-                setUpdatedProduct({
-                  ...updatedProduct,
-                  features: e.target.value,
-                })
-              }
-            />
+                <input
+                  className="border p-2"
+                  placeholder="Description"
+                  value={updatedProduct.description}
+                  onChange={(e) =>
+                    setUpdatedProduct({
+                      ...updatedProduct,
+                      description: e.target.value,
+                    })
+                  }
+                />
 
-            <input
-              className="border p-2"
-              placeholder="Description"
-              value={updatedProduct.description}
-              onChange={(e) =>
-                setUpdatedProduct({
-                  ...updatedProduct,
-                  description: e.target.value,
-                })
-              }
-            />
-            <input
-              className="border p-2"
-              placeholder="Brand"
-              value={updatedProduct.brand}
-              onChange={(e) =>
-                setUpdatedProduct({ ...updatedProduct, brand: e.target.value })
-              }
-            />
-          </div>
+                <img src={updatedProduct.imageUrl} className="w-40" alt="" />
 
-          <button
-            className="mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
-            onClick={updateProduct}
-          >
-            Update Product
-          </button>
+                <input
+                  className="border p-2"
+                  placeholder="Brand"
+                  value={updatedProduct.brand}
+                  onChange={(e) =>
+                    setUpdatedProduct({
+                      ...updatedProduct,
+                      brand: e.target.value,
+                    })
+                  }
+                />
+              </div>
+
+              <button
+                className="mt-4 bg-blue-600 text-white px-4 py-2 rounded cursor-pointer active:scale-90 hover:bg-blue-700"
+                onClick={updateProduct}
+              >
+                Update Product
+              </button>
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 };
